7477fb680cecc01af9f3ba0104247041227062dc
Melissa <melissahentona@usp.br>
2022-08-14 15:22:14 -0300

    versao com confirmacao

--- script.js
+++ script.js
@@ -73,27 +73,74 @@ function liberarFinalizar(){
     }
 }
 
-function envioWpp(){
+let precoPratoNumero;
+let precoBebidaNumero;
+let precoSobremesaNumero;
+let precoFinal;
+function confirmarPedido(){
     if (prato !== undefined && bebida !== undefined && sobremesa !== undefined){
+        //abrir a guia do pedido
+        const confirmacao = document.querySelector('.confirmar');
+        confirmacao.classList.add('aparece');
+        console.log(confirmacao)
+        //ligar o background
+        const background = document.querySelector('.fundobranco');
+        background.classList.add('aparece');
+
+        //mostrar o pedido correto
         let precoPratoSemRS = precoPrato.replace('R$','');
         let precoPratoCorrigido = precoPratoSemRS.replace(',','.');
-        let precoPratoNumero = Number(precoPratoCorrigido);
+        precoPratoNumero = Number(precoPratoCorrigido);
         let precoBebidaSemRS = precoBebida.replace('R$','');
         let precoBebidaCorrigido = precoBebidaSemRS.replace(',','.');
-        let precoBebidaNumero = Number(precoBebidaCorrigido);
+        precoBebidaNumero = Number(precoBebidaCorrigido);
         let precoSobremesaSemRS = precoPrato.replace('R$','');
         let precoSobremesaCorrigido = precoSobremesaSemRS.replace(',','.');
-        let precoSobremesaNumero = Number(precoSobremesaCorrigido);
+        precoSobremesaNumero = Number(precoSobremesaCorrigido);
         const soma = precoPratoNumero + precoBebidaNumero + precoSobremesaNumero;
-        const precoFinal = soma.toFixed(2);
+        precoFinal = soma.toFixed(2);
 
-        let mensagem = `Olá, gostaria de fazer o pedido: \n
-        - Prato: ${prato}\n
-        - Bebida: ${bebida}\n
-        - Sobremesa: ${sobremesa}\n
-        Total: R$ ${precoFinal}`
+        const pedido = document.querySelector('.alinhar2');
 
-        let link = 'https://wa.me/?text='+encodeURIComponent(mensagem)
-        window.open(link);
+        const pedidoPrato = pedido.children[1]
+        const pedidoNomePrato = pedidoPrato.children[0]
+        const pedidoPrecoPrato = pedidoPrato.children[1]
+        pedidoNomePrato.innerHTML = `${prato}`
+        pedidoPrecoPrato.innerHTML = `${precoPratoNumero.toFixed(2)}`
+
+        const pedidoBebida = pedido.children[2]
+        const pedidoNomeBebida = pedidoBebida.children[0]
+        const pedidoPrecoBebida = pedidoBebida.children[1]
+        pedidoNomeBebida.innerHTML = `${bebida}`
+        pedidoPrecoBebida.innerHTML = `${precoBebidaNumero.toFixed(2)}`
+
+        const pedidoSobremesa = pedido.children[3]
+        const pedidoNomeSobremesa = pedidoSobremesa.children[0]
+        const pedidoPrecoSobremesa = pedidoSobremesa.children[1]
+        pedidoNomeSobremesa.innerHTML = `${sobremesa}`
+        pedidoPrecoSobremesa.innerHTML = `${precoSobremesaNumero.toFixed(2)}`
+
+        const pedidoTotal = pedido.children[4]
+        const pedidoPrecoTotal = pedidoTotal.children[1]
+        pedidoPrecoTotal.innerHTML = `${precoFinal}`
     }
+}
+function envioWpp(){
+    let mensagem = `Olá, gostaria de fazer o pedido: \n
+    - Prato: ${prato}\n
+    - Bebida: ${bebida}\n
+    - Sobremesa: ${sobremesa}\n
+    Total: R$ ${precoFinal}`
+    let link = 'https://wa.me/5511987067348?text='+encodeURIComponent(mensagem)
+    window.open(link);
+    }
+
+function fechar(){
+    //fechar a guia do pedido
+    const confirmacao = document.querySelector('.confirmar');
+    confirmacao.classList.remove('aparece');
+    console.log(confirmacao)
+    //fechar o background
+    const background = document.querySelector('.fundobranco');
+    background.classList.remove('aparece');
 }
